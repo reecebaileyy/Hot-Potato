24a19cbc3f3730b1b7ffcde29e42fc2a0b99ff7d
Rbailey8857 <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-05-09 16:55:53 -0700

    Restart

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>zkPotato</title>
+        <title>HOT POTATO</title>
         <meta name="description" content="Hot Potato Hot Potato..."/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico"/>
