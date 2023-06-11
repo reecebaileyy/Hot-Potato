74477d729230039ae8159d2c37e56bdeafb0730a
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-06-11 15:12:40 -0700

    Update displayImage.js

--- frontend/components/displayImage.js
+++ frontend/components/displayImage.js
@@ -1,7 +1,10 @@
-//components/displayImage.js
 import React, { useEffect } from 'react';
 import { useContractRead } from 'wagmi';
 import Image from 'next/image';
+
+const OptimizedImage = (props) => (
+  <Image {...props} unoptimized={true} />
+);
 
 const TokenImage = ({ tokenId, ABI }) => {
   const { data: getImageString, isLoading, refetch: refetchImageString, isError } = useContractRead({
@@ -12,36 +15,51 @@ const TokenImage = ({ tokenId, ABI }) => {
     enabled: true
   });
 
+  const { data: getActiveTokens, isLoading: loadingActiveTokens, refetch: refetchGetActiveTokens } = useContractRead({
+    address: '0xAA065769Df8AFb40dbD7d987f6ec6B35Db18b303',
+    abi: ABI,
+    functionName: 'getActiveTokens',
+    enabled: false,
+  });
+  const _activeTokens = parseInt(getActiveTokens, 10);
+
   const { data: potatoTokenId, isLoading: loadingPotatoTokenId, refetch: refetchPotatoTokenId } = useContractRead({
     address: '0xAA065769Df8AFb40dbD7d987f6ec6B35Db18b303',
     abi: ABI,
     functionName: 'potatoTokenId',
     enabled: true
-  })
+  });
   const _potatoTokenId = parseInt(potatoTokenId, 10);
 
   // Refetch image whenever tokenId or ABI changes
   useEffect(() => {
     refetchImageString();
     refetchPotatoTokenId();
-  }, []);
+  }, [_activeTokens]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (isError || !getImageString) {
-    return <div>Error loading image. <button onClick={() => refetchImageString({ args: [tokenId] })} >Try Refreshing</button></div>;
+    return (
+      <div>
+        Error loading image.{' '}
+        <button onClick={() => refetchImageString({ args: [tokenId] })}>
+          Try Refreshing
+        </button>
+      </div>
+    );
   }
 
   return (
-    <div className={`${tokenId == _potatoTokenId ? ' flex flex-col animate-pulse' : 'flex flex-col'}`} key={tokenId} >
-        <Image
-          src={`data:image/svg+xml,${encodeURIComponent(getImageString)}`}
-          width={500}
-          height={500}
-          alt={`Token ${tokenId} Image`}
-        />
+    <div className={`${tokenId == _potatoTokenId ? ' flex flex-col animate-pulse' : 'flex flex-col'}`} key={tokenId}>
+      <OptimizedImage
+        src={`data:image/svg+xml,${encodeURIComponent(getImageString)}`}
+        width={500}
+        height={500}
+        alt={`Token ${tokenId} Image`}
+      />
       Token ID: {tokenId}
       <button onClick={() => refetchImageString({ args: [tokenId] })}>Refresh Image</button>
     </div>
