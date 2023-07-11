191926f946587a4543c1f2abe71b46c98f27e207
Rbailey8857 <80913719+reecebaileyy@users.noreply.github.com>
2023-07-10 20:40:12 -0700

    Start on leaderboard server

--- server/update-passes.js
+++ server/update-passes.js
@@ -1,16 +1,21 @@
-// pages/api/update-passes.js
+const express = require('express');
 const prisma = require('./lib/prisma/index.js');
 const ABI = require('./abi/UNKNOWN.json');
 
-async function handler() {
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function handler(req, res) {
   const { useContractEvent } = await import('wagmi');
+  
   useContractEvent({
     address: '0xb6f6CE3AD79c658645682169C0584664cfEc7908',
     abi: ABI,
     eventName: 'SuccessfulPass',
     async listener(log) {
       try {
-        const player = log[0]?.args?.player;  
+        const player = log[0]?.args?.player;
+        
         await prisma.leaderboard.upsert({
             where: { address: player },
             update: {
@@ -30,10 +35,17 @@ async function handler() {
 
       } catch (error) {
         console.error('Error updating successful passes', error);
+        res.status(500).send('An error occurred');
       }
     },
   });
+
+  res.status(200).send('Request received and processing');
 }
 
-module.exports = handler;
+// Using your handler function when a GET request is made to the home page
+app.get('/', handler);
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
